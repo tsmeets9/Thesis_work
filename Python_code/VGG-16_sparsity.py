3a416a1acd87d6a12352e88a0953901708640249
tsmeets9 <47253276+tsmeets9@users.noreply.github.com>
2023-06-06 09:40:33 +0200

    Corrected VGG-16 sparsity script

--- Python_code/VGG-16_sparsity.py
+++ Python_code/VGG-16_sparsity.py
@@ -5,23 +5,26 @@ from keras.applications.vgg16 import VGG16
 from keras.applications.vgg16 import preprocess_input
 from keras.utils import load_img, img_to_array
 from keras.models import Model
+import numpy as np
 from numpy import expand_dims
-import tensorflow as tf
-import numpy as np 
 # load the model
 model = VGG16()
 # summarize the model
 model.summary()
-import matplotlib
-from matplotlib import pyplot
-import matplotlib.pylab as plt
-import scipy.sparse as sparse 
-print(matplotlib.font_manager.get_font_names())
+from matplotlib import pyplot as plt
 
-model = Model(inputs=model.inputs, outputs=model.layers[5].output)
+for i in range(len(model.layers)):
+    layer = model.layers[i]
+    if 'conv' in layer.name:
+        print(i, layer.name, layer.output.shape)
+
+ixs = [2, 5, 9, 13, 17]
+outputs = [model.layers[i].output for i in ixs]
+model = Model(inputs = model.inputs, outputs = outputs)
 model.summary()
+
 script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
-rel_path = "images/bird.jpg"
+rel_path = "images/elephant.jpg"
 img_path = os.path.join(script_dir, rel_path)
 img = load_img(img_path, target_size=(224, 224))
 
@@ -29,15 +32,24 @@ img = img_to_array(img)
 img = expand_dims(img, axis=0)
 
 img = preprocess_input(img)
-feature_maps = model.predict(img)
-for i in range(15):
-    feature_map = feature_maps[0, :, :, i]
-    plt.spy(feature_map)
-    plt.xticks(fontsize=10.5)
-    plt.yticks(fontsize=10.5)
 
-    plt.show()
-    array_size = feature_map.size
-    nb_zeros_array = np.count_nonzero(feature_map==0)
+feature_maps = model.predict(img)
+
+for fmap in feature_maps: 
+    print(fmap.shape)
+    print(type(fmap))
+
+    to_plot = np.ndarray(0)
+    array_size = 0
+    nb_zeros_array = 0
+    for i in range(fmap.shape[3]):
+        array_size += fmap[0, :, :, i].size
+        nb_zeros_array += np.count_nonzero(fmap[0, :, :, i] == 0)
     sparsity = nb_zeros_array/array_size
     print(sparsity)
+
+    plt.spy(fmap[0, :, :, 5])
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+
+    plt.show()
